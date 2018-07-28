9527664f9a50e038cc0793aea22dfc03896de59e
Santos Rodrigo Abadia Heredia <rodrigoabadia@mbp-de-santos.home>
2018-07-28 12:37:19 +0200

    mi primer commit

---
+++ input_statement.py
@@ -0,0 +1,56 @@
+"""message = input("Tell me something, and I will repeat it back to you: ")
+print(message)"""
+
+########################################################################################
+
+'''name = input("Please enter your name: ")
+print("Hi, " + name.title() + "!")'''
+
+########################################################################################
+
+"""prompt = "If you tell us who you are, we can personalize the message you see."
+prompt += "\nWhat is your first name? "
+
+name = input(prompt)
+print("\nHello, " + name.title() + "!")"""
+
+########################################################################################
+
+'''age = input("How old are you? ")
+age = int(age)
+print(age >= 18) '''
+
+########################################################################################
+
+'''height = input("How tall are you, in inches? ")
+height = int(height)
+
+if height >= 36:
+	print("\nYou're tall enough to ride!")
+else:
+	print("\nYou'll be able to ride when you're a little older.")'''
+
+########################################################################################
+
+"""prompt = "\nEven or Odd number."
+prompt += "\nPlease, enter a number: "
+
+odd = input(prompt)
+odd = int(odd)
+
+if odd % 2 == 0:
+	print("---> " + str(odd) + " is an even number.")
+else:
+	print("---> " + str(odd) + " is an odd number.")"""
+
+########################################################################################
+
+"""people = input("\nHow many people are going to dinner toningth?\n ")
+people = int(people)
+
+if people <= 8:
+	print("There is an avilable table, wait for the waitress please")
+else:
+	print("You have to wait for an avilable table, sorry")""" 
+
+########################################################################################
